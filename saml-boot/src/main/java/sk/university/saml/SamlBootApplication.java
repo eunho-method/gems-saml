b87dc1f6fa1e6634efb7fd59839bb0b4deec34c4
Luna <yeon@catbus.xyz>
2022-10-23 13:36:45 +0900

    ..

---
+++ saml-boot/src/main/java/sk/university/saml/SamlBootApplication.java
@@ -0,0 +1,10 @@
+package sk.university.saml;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SamlBootApplication {
+
+    public static void main(String[] args){
+        SpringBootApplication.run(SamlBootApplication.class, args);
+    }
+}
